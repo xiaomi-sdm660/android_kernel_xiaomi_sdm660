3d72f002a21fc19cbfe7b365e871609038814998
Sami Tolvanen <samitolvanen@google.com>
2017-06-16 12:52:57 -0700

    FROMLIST: BACKPORT: kbuild: fix dynamic ftrace with clang LTO
    With CONFIG_LTO_CLANG enabled, LLVM IR won't be compiled into object
    files until modpost_link. This change postpones calls to recordmcount
    until after this step.
    
    In order to exclude ftrace_process_locs from inspection, we add a new
    code section .text..ftrace, which we tell recordmcount to ignore, and
    a __norecordmcount attribute for moving functions to this section.
    
    Bug: 62093296
    Bug: 67506682
    Change-Id: Iba2c053968206acf533fadab1eb34a743b5088ee
    (am from https://patchwork.kernel.org/patch/10060327/)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -467,6 +467,10 @@ unsigned long read_word_at_a_time(const void *addr)
 #define __visible
 #endif
 
+#ifndef __norecordmcount
+#define __norecordmcount
+#endif
+
 /*
  * Assume alignment of return value.
  */
