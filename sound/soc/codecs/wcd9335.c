67b0c6c395f245deb7667de79a669f9eceafb182
BlackSoulxxx <duyquangdns@gmail.com>
2017-10-06 11:56:56 +0530

    ASoC:WCD9335: Enhance SLimbus CLK Gear
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- sound/soc/codecs/wcd9335.c
+++ sound/soc/codecs/wcd9335.c
@@ -94,7 +94,7 @@
 	(TASHA_CPE_SS_ERR_STATUS_WDOG_BITE | \
 	 TASHA_CPE_SS_ERR_STATUS_MEM_ACCESS)
 
-#define SLIM_BW_CLK_GEAR_9 6200000
+#define SLIM_BW_CLK_GEAR_10 24600000
 #define SLIM_BW_UNVOTE 0
 
 #define CPE_FLL_CLK_75MHZ 75000000
@@ -13325,7 +13325,7 @@ static int tasha_codec_vote_max_bw(struct snd_soc_codec *codec,
 	if (vote) {
 		tasha->ref_count++;
 		if (tasha->ref_count == 1) {
-			bw_ops = SLIM_BW_CLK_GEAR_9;
+			bw_ops = SLIM_BW_CLK_GEAR_10;
 			tasha_codec_slim_reserve_bw(codec,
 				bw_ops, true);
 		}
