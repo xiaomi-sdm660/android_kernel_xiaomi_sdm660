3157aefc04889c8519461dcd508e7421332abae8
Patrick Bellasi <patrick.bellasi@arm.com>
2018-03-09 09:52:43 +0000

    ANDROID: sched/fair: Cleanup cpu_util{_wake}()
    The current implementation of cpu_util and cpu_util_wake makes more
    difficult the backporting of mainline patches and it also has some
    features not longer required by the current EAS code, e.g. delta
    utilization in __cpu_util().
    
    Let's clean up these functions definitions to:
    1. get rid of the not longer required __cpu_util(cpu, delta)
       This function is now only called with delta=0 and thus we can refold
       its implementation into the original wrapper function cpu_util(cpu)
    2. optimize for the WALT path on CONFIG_SCHED_WALT builds
       Currently indeed we execute some not necessary PELT related code even
       when WALT signals are required.
       Let's change this by assuming that on CONFIG_SCHED_WALT build we are
       likely using WALT signals. While on !CONFIG_SCHED_WALT we still have
       just the PELT signals with a code structure which matches mainline
    3. move the definitions from sched/sched.h into sched/fair.c
       This is the only module using these functions and it also better
       align with the mainline location for these functions
    4. get rid of the walt_util macro
       That macro has a function-like signature but it's modifying a
       parameter (passed by value) which makes it a bit confusing.
       Moreover, the usage of the min_t() macro to cap signals with
       capacity_orig_of() it makes not more required the explicit type cast
       used by that macro to support both 32 and 63 bits targets.
    5. remove forward declarations
       Which are not required once the definition is moved at the top of
       fair.c, since they don't have other local dependencies.
    
    [diepquynh]: Backport from 4.14
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Change-Id: I61c9b7b8a0a34b494527c5aa76218c64543c16d2
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1638,69 +1638,19 @@ extern unsigned int sysctl_sched_use_walt_cpu_util;
 extern unsigned int walt_ravg_window;
 extern bool walt_disabled;
 
-/*
- * cpu_util returns the amount of capacity of a CPU that is used by CFS
- * tasks. The unit of the return value must be the one of capacity so we can
- * compare the utilization with the capacity of the CPU that is available for
- * CFS task (ie cpu_capacity).
- *
- * cfs_rq.avg.util_avg is the sum of running time of runnable tasks plus the
- * recent utilization of currently non-runnable tasks on a CPU. It represents
- * the amount of utilization of a CPU in the range [0..capacity_orig] where
- * capacity_orig is the cpu_capacity available at the highest frequency
- * (arch_scale_freq_capacity()).
- * The utilization of a CPU converges towards a sum equal to or less than the
- * current capacity (capacity_curr <= capacity_orig) of the CPU because it is
- * the running time on this CPU scaled by capacity_curr.
- *
- * Nevertheless, cfs_rq.avg.util_avg can be higher than capacity_curr or even
- * higher than capacity_orig because of unfortunate rounding in
- * cfs.avg.util_avg or just after migrating tasks and new task wakeups until
- * the average stabilizes with the new running time. We need to check that the
- * utilization stays within the range of [0..capacity_orig] and cap it if
- * necessary. Without utilization capping, a group could be seen as overloaded
- * (CPU0 utilization at 121% + CPU1 utilization at 80%) whereas CPU1 has 20% of
- * available capacity. We allow utilization to overshoot capacity_curr (but not
- * capacity_orig) as it useful for predicting the capacity required after task
- * migrations (scheduler-driven DVFS).
- */
-static inline unsigned long __cpu_util(int cpu, int delta)
+static inline unsigned long task_util(struct task_struct *p)
 {
-	unsigned long util = cpu_rq(cpu)->cfs.avg.util_avg;
-	unsigned long capacity = capacity_orig_of(cpu);
 
 #ifdef CONFIG_SCHED_WALT
-	if (!walt_disabled && sysctl_sched_use_walt_cpu_util)
-		util = div64_u64(cpu_rq(cpu)->cumulative_runnable_avg,
-				 walt_ravg_window >> SCHED_LOAD_SHIFT);
+	if (!walt_disabled && sysctl_sched_use_walt_task_util) {
+		unsigned long demand = p->ravg.demand;
+		return (demand << SCHED_CAPACITY_SHIFT) / walt_ravg_window;
+	}
 #endif
-
-	delta += util;
-	if (delta < 0)
-		return 0;
-
-	return (delta >= capacity) ? capacity : delta;
+	return p->se.avg.util_avg;
 }
 
-static inline unsigned long cpu_util(int cpu)
-{
-	return __cpu_util(cpu, 0);
-}
-
-static inline unsigned long cpu_util_freq(int cpu)
-{
-	unsigned long util = cpu_rq(cpu)->cfs.avg.util_avg;
-	unsigned long capacity = capacity_orig_of(cpu);
-
-#ifdef CONFIG_SCHED_WALT
-	if (!walt_disabled && sysctl_sched_use_walt_cpu_util)
-		util = div64_u64(cpu_rq(cpu)->prev_runnable_sum,
-				 walt_ravg_window >> SCHED_LOAD_SHIFT);
-#endif
-	return (util >= capacity) ? capacity : util;
-}
-
-#endif
+#endif /* CONFIG_SMP */
 
 static inline void sched_rt_avg_update(struct rq *rq, u64 rt_delta)
 {
