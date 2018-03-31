ba6e238d6904099503907072cdb10ed175c3542c
Demon Singur <demonsingur@gmail.com>
2018-03-31 10:56:42 +0000

    synaptics_dsx_force: guard test reporting
    Change-Id: I4cae16efcae5d1fa7aa1b1a6f7851e4fe9200edd
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_core.h
+++ drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_core.h
@@ -398,8 +398,11 @@ struct synaptics_rmi4_data {
 	bool chip_is_tddi;
 	bool open_test_b7;
 	bool short_test_extend;
+
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_DSX_TEST_REPORTING_FORCE
 	bool disable_data_dump;
 	bool dump_flags;
+#endif
 	int (*reset_device)(struct synaptics_rmi4_data *rmi4_data,
 			bool rebuild);
 	int (*irq_enable)(struct synaptics_rmi4_data *rmi4_data, bool enable,
@@ -413,7 +416,9 @@ struct synaptics_rmi4_data {
 	struct pinctrl_state *pinctrl_state_suspend;
 	struct synaptics_dsx_factory_param *factory_param;
 
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_DSX_TEST_REPORTING_FORCE
 	struct completion dump_completion;
+#endif
 #ifdef CONFIG_TOUCH_DEBUG_FS
 	struct dentry *debugfs;
 #endif
