87fa2c35125f9b3eba34a7283c27fd8d26e11b1e
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2018-06-15 23:35:48 -0300

    Optmize audio codec!! -> Better audio quality; -> Louder; -> Clean; -> Safe.
    Modified!
    
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -60,7 +60,7 @@
 #define FW_READ_ATTEMPTS 15
 #define FW_READ_TIMEOUT 4000000
 #define FAKE_REM_RETRY_ATTEMPTS 3
-#define MAX_IMPED 60000
+#define MAX_IMPED 100000
 
 
 #if defined(CONFIG_MACH_XIAOMI_WAYNE)
@@ -3060,6 +3060,7 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 		      bool impedance_det_en)
 {
 	int ret = 0;
+	impedance_det_en = true;
 	int hph_swh = 0;
 	int gnd_swh = 0;
 	u32 hph_moist_config[3];
