75369113e779e2e554330e9a18b5958cb04f61a1
Francisco Franco <franciscofranco.1990@gmail.com>
2018-06-27 00:41:07 +0000

    cpu_input_boost: make the userspace module param vars static
    According to Sultan without them being static it pollutes the
    global symbol list and reduces the compiler's ability to optimize.
    
    Reported-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -20,9 +20,9 @@
 #include <linux/moduleparam.h>
 #include <linux/slab.h>
 
-unsigned int input_boost_freq_lp = CONFIG_INPUT_BOOST_FREQ_LP;
-unsigned int input_boost_freq_hp = CONFIG_INPUT_BOOST_FREQ_PERF;
-unsigned short input_boost_duration = CONFIG_INPUT_BOOST_DURATION_MS;
+static unsigned int input_boost_freq_lp = CONFIG_INPUT_BOOST_FREQ_LP;
+static unsigned int input_boost_freq_hp = CONFIG_INPUT_BOOST_FREQ_PERF;
+static unsigned short input_boost_duration = CONFIG_INPUT_BOOST_DURATION_MS;
 
 module_param(input_boost_freq_lp, uint, 0644);
 module_param(input_boost_freq_hp, uint, 0644);
