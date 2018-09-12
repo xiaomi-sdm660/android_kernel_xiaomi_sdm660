acafd3097fa11cfcff51d662073e1d8d0b861721
Tyler Nijmeh <tylernij@gmail.com>
2018-09-11 20:28:16 -0700

    block: Introduce Anxiety I/O Scheduler
    The purpose of Anxiety is to improve throughput on latency-sensitive
    tasks on embedded flash storage devices. On modern devices, embedded
    systems contain relatively fast storage, so spending too much time with
    complicated queue-shifting scheduling algorithms may end up
    bottlenecking the device. Similarly to the No-op I/O scheduler, Anxiety
    works with a FIFO queue; however, Anxiety includes basic request logic
    based on READ v. WRITE requests. WRITE requests are starved at a fixed
    ratio, defined as the `max_writes_starved` tunable. READ requests are
    given priority over WRITE requests due to their essentiality in
    determining load-time latency.
    
    ------------------TEST--------------------
    SCHEDULERS	Anxiety		CFQ
    S. READ		286.22		280.74
    S. WRITE	221.01		174.0
    R. READ		41.35		47.89
    R. WRITE	10.65		10.7
    SQL INS.	1276.57		1121.25
    SQL UPD.	2920.97		2978.74
    SQL DEL.	2503.16		2642.3
    ------------------------------------------
    
    Tests were done using AndroBench on a Xiaomi Mi A2 on the latest 4.4.178
    patch. This application has been known to be misrepresentative on I/O
    scores in the past.
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ block/anxiety-iosched.c
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2019, Tyler Nijmeh <tylernij@gmail.com>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/blkdev.h>
+#include <linux/elevator.h>
+#include <linux/bio.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+
+/* Default tunable values */
+#define	DEFAULT_MAX_WRITES_STARVED (2)	/* Max times reads can starve a write */
+
+struct anxiety_data {
+	struct list_head queue[2];
+	uint16_t writes_starved;
+
+	/* Tunables */
+	uint8_t max_writes_starved;
+};
+
+static void anxiety_merged_requests(struct request_queue *q, struct request *rq, struct request *next)
+{
+	rq_fifo_clear(next);
+}
+
+static inline struct request *anxiety_choose_request(struct anxiety_data *adata)
+{
+	/* Prioritize reads unless writes are exceedingly starved */
+	bool starved = adata->writes_starved > adata->max_writes_starved;
+
+	/* Handle a read request */
+	if (!starved && !list_empty(&adata->queue[READ])) {
+		adata->writes_starved++;
+		return rq_entry_fifo(adata->queue[READ].next);
+	}
+
+	/* Handle a write request */
+	if (!list_empty(&adata->queue[WRITE])) {
+		adata->writes_starved = 0;
+		return rq_entry_fifo(adata->queue[WRITE].next);
+	}
+
+	/* If there are no requests, then there is nothing to starve */
+	adata->writes_starved = 0;
+	return NULL;
+}
+
+static int anxiety_dispatch(struct request_queue *q, int force)
+{
+	struct request *rq = anxiety_choose_request(q->elevator->elevator_data);
+
+	if (!rq)
+		return 0;
+
+	rq_fifo_clear(rq);
+	elv_dispatch_add_tail(rq->q, rq);
+
+	return 1;
+}
+
+static void anxiety_add_request(struct request_queue *q, struct request *rq)
+{
+	const uint8_t dir = rq_data_dir(rq);
+
+	list_add_tail(&rq->queuelist, &((struct anxiety_data *) q->elevator->elevator_data)->queue[dir]);
+}
+
+static struct request *anxiety_former_request(struct request_queue *q, struct request *rq)
+{
+	const uint8_t dir = rq_data_dir(rq);
+
+	if (rq->queuelist.prev == &((struct anxiety_data *) q->elevator->elevator_data)->queue[dir])
+		return NULL;
+
+	return list_prev_entry(rq, queuelist);
+}
+
+static struct request *anxiety_latter_request(struct request_queue *q, struct request *rq)
+{
+	const uint8_t dir = rq_data_dir(rq);
+
+	if (rq->queuelist.next == &((struct anxiety_data *) q->elevator->elevator_data)->queue[dir])
+		return NULL;
+
+	return list_next_entry(rq, queuelist);
+}
+
+static int anxiety_init_queue(struct request_queue *q, struct elevator_type *elv)
+{
+	struct anxiety_data *adata;
+	struct elevator_queue *eq = elevator_alloc(q, elv);
+
+	if (!eq)
+		return -ENOMEM;
+
+	/* Allocate the data */
+	adata = kmalloc_node(sizeof(*adata), GFP_KERNEL, q->node);
+	if (!adata) {
+		kobject_put(&eq->kobj);
+		return -ENOMEM;
+	}
+
+	/* Set the elevator data */
+	eq->elevator_data = adata;
+
+	/* Initialize */
+	INIT_LIST_HEAD(&adata->queue[READ]);
+	INIT_LIST_HEAD(&adata->queue[WRITE]);
+	adata->writes_starved = 0;
+	adata->max_writes_starved = DEFAULT_MAX_WRITES_STARVED;
+
+	/* Set elevator to Anxiety */
+	spin_lock_irq(q->queue_lock);
+	q->elevator = eq;
+	spin_unlock_irq(q->queue_lock);
+
+	return 0;
+}
+
+/* Sysfs access */
+static ssize_t anxiety_max_writes_starved_show(struct elevator_queue *e, char *page)
+{
+	struct anxiety_data *adata = e->elevator_data;
+
+	return snprintf(page, PAGE_SIZE, "%u\n", adata->max_writes_starved);
+}
+
+static ssize_t anxiety_max_writes_starved_store(struct elevator_queue *e, const char *page, size_t count)
+{
+	struct anxiety_data *adata = e->elevator_data;
+	int ret;
+
+	ret = kstrtou8(page, 0, &adata->max_writes_starved);
+	if (ret < 0)
+		return ret;
+
+	return count;
+}
+
+static struct elv_fs_entry anxiety_attrs[] = {
+	__ATTR(max_writes_starved, 0644, anxiety_max_writes_starved_show, anxiety_max_writes_starved_store),
+	__ATTR_NULL
+};
+
+static struct elevator_type elevator_anxiety = {
+	.ops = {
+		.elevator_merge_req_fn	= anxiety_merged_requests,
+		.elevator_dispatch_fn	= anxiety_dispatch,
+		.elevator_add_req_fn	= anxiety_add_request,
+		.elevator_former_req_fn	= anxiety_former_request,
+		.elevator_latter_req_fn	= anxiety_latter_request,
+		.elevator_init_fn	= anxiety_init_queue,
+	},
+	.elevator_name = "anxiety",
+	.elevator_attrs = anxiety_attrs,
+	.elevator_owner = THIS_MODULE,
+};
+
+static int __init anxiety_init(void)
+{
+	return elv_register(&elevator_anxiety);
+}
+
+static void __exit anxiety_exit(void)
+{
+	elv_unregister(&elevator_anxiety);
+}
+
+module_init(anxiety_init);
+module_exit(anxiety_exit);
+
+MODULE_AUTHOR("Tyler Nijmeh");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Anxiety IO scheduler");
