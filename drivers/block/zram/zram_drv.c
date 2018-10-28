a802621286f7bf78c0bdf29ead6c849e1ba58083
Nathan Chancellor <natechancellor@gmail.com>
2018-10-28 10:09:32 -0700

    zram: Do not allow compression algorithm to be changed
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -792,6 +792,7 @@ static ssize_t comp_algorithm_show(struct device *dev,
 static ssize_t comp_algorithm_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
+#if 0
 	struct zram *zram = dev_to_zram(dev);
 	char compressor[ARRAY_SIZE(zram->compressor)];
 	size_t sz;
@@ -814,6 +815,7 @@ static ssize_t comp_algorithm_store(struct device *dev,
 
 	strcpy(zram->compressor, compressor);
 	up_write(&zram->init_lock);
+#endif
 	return len;
 }
 
