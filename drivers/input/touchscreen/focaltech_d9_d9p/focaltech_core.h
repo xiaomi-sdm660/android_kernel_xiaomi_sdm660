0cb4b9b0587f4c392a064486a9e5e4d8857dc14e
Park Ju Hyung <qkrwngud825@gmail.com>
2018-12-24 01:17:40 +0900

    focaltech: optimize gesture handling
     - Inline bunch of functions
     - Remove unnecessary mode/active members and replace it with a single boolean
     - Use enable_irq_wake() for proper gesture handlings
       Why wasn't this done by default?
       This fixes gestures working unreliably on suspend
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- drivers/input/touchscreen/focaltech_d9_d9p/focaltech_core.h
+++ drivers/input/touchscreen/focaltech_d9_d9p/focaltech_core.h
@@ -169,18 +169,6 @@ struct fts_ts_data {
 extern struct fts_ts_data *fts_data;
 extern u8 fts_chipid;
 
-/* Gesture functions */
-#if FTS_GESTURE_EN
-int fts_gesture_init(struct fts_ts_data *ts_data);
-int fts_gesture_exit(struct i2c_client *client);
-void fts_gesture_recovery(struct i2c_client *client);
-int fts_gesture_readdata(struct fts_ts_data *ts_data);
-int fts_gesture_suspend(struct i2c_client *i2c_client);
-int fts_gesture_resume(struct i2c_client *client);
-int fts_input_event(struct input_dev *dev, unsigned int type, unsigned int code, int value);
-
-#endif
-
 /* Apk and functions */
 #if FTS_APK_NODE_EN
 int fts_create_apk_debug_channel(struct fts_ts_data *);
