9061f46cbe8f0ab81d0cdea6ae82be8687db5468
Tyler Nijmeh <tylernij@gmail.com>
2019-02-02 12:31:52 -0800

    sched: schedutil: Tune rate limits
    We don't want to manipulate the CPU frequency so often.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -26,8 +26,8 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_driver_fast_switch(x, y) 0
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
-#define UP_RATE_LIMIT_US			(1000)
-#define DOWN_RATE_LIMIT_US			(1000)
+#define UP_RATE_LIMIT_US			(500)
+#define DOWN_RATE_LIMIT_US			(20000)
 #define SUGOV_KTHREAD_PRIORITY	50
 
 struct sugov_tunables {
