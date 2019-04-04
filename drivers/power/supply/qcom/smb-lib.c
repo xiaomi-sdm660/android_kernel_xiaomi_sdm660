b2300552591aac5ad83351accf2085b0d9b1e401
mracar07 <umutcan@umutcanacar.me>
2019-04-04 19:09:17 +0000

    Smb: remove debug message for smblib_handle
    [  165.776112] PMI: smblib_handle_debug: IRQ: input-current-limiting
    
    Thanks to @YaroST12
    
    Change-Id: I11434a27e221cd4a9e69bf946225df5f77940f8e
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -3415,7 +3415,6 @@ irqreturn_t smblib_handle_debug(int irq, void *data)
 	struct smb_irq_data *irq_data = data;
 	struct smb_charger *chg = irq_data->parent_data;
 
-	smblib_dbg(chg, PR_INTERRUPT, "IRQ: %s\n", irq_data->name);
 	return IRQ_HANDLED;
 }
 
