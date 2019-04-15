a0d40a6f0eba4ad0f1abc5cb7efa969365a27fdb
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 17:48:26 -0700

    msm: mdss: Fix compile errors when debugfs is disabled
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/video/fbdev/msm/mdss_dsi.h
+++ drivers/video/fbdev/msm/mdss_dsi.h
@@ -726,7 +726,13 @@ void mdss_dsi_set_reg(struct mdss_dsi_ctrl_pdata *ctrl, int off,
 int mdss_dsi_phy_pll_reset_status(struct mdss_dsi_ctrl_pdata *ctrl);
 int mdss_dsi_check_panel_status(struct mdss_dsi_ctrl_pdata *ctrl, void *arg);
 
+#ifdef CONFIG_DEBUG_FS
 void mdss_dsi_debug_bus_init(struct mdss_dsi_data *sdata);
+#else
+static inline void mdss_dsi_debug_bus_init(struct mdss_dsi_data *sdata)
+{
+}
+#endif
 
 static inline const char *__mdss_dsi_pm_name(enum dsi_pm_type module)
 {
