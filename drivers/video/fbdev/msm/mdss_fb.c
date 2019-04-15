442f8135d35338efa9b873ac32ba304449179260
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 21:20:56 -0700

    msm: mdss: Boost CPU and DDR bus when committing a new frame
    In order to reduce jank, request a CPU and DDR bus boost whenever a new
    frame is ready to be rendered to the display. The boosts should be
    sufficient enough to render 60 FPS without any dropped frames when there
    is no significant external source of load.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -47,6 +47,9 @@
 #include <linux/file.h>
 #include <linux/kthread.h>
 #include <linux/dma-buf.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
+
 #include <linux/mdss_io_util.h>
 #include <linux/wakelock.h>
 #include <sync.h>
@@ -560,7 +563,6 @@ static void __mdss_fb_idle_notify_work(struct work_struct *work)
 		sysfs_notify(&mfd->fbi->dev->kobj, NULL, "idle_notify");
 	mfd->idle_state = MDSS_FB_IDLE;
 }
-
 
 static ssize_t mdss_fb_get_fps_info(struct device *dev,
 		struct device_attribute *attr, char *buf)
@@ -5564,6 +5566,8 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+		cpu_input_boost_kick();
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
