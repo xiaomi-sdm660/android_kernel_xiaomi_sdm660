47e2c980b58ab2d3aab37d4063f68a1f335a2d7e
Max Weffers <rcstar6696@gmail.com>
2019-04-18 10:38:34 +0200

    KLAPSE: adapt for sdm660
    Signed-off-by: srfarias <erikofarias@gmail.com> (cherry picked from 
    commit afae0a1365afd5d6b0d07371b0f80fc352eb2ee8)

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -61,6 +61,10 @@
 #include "mdss_mdp.h"
 #include "dsi_access.h"
 
+#ifdef CONFIG_KLAPSE
+#include "klapse.h"
+#endif
+
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -340,6 +344,10 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 		mutex_unlock(&mfd->bl_lock);
 	}
 	mfd->bl_level_usr = bl_lvl;
+
+#ifdef CONFIG_KLAPSE
+	set_rgb_slider(bl_lvl);
+#endif
 }
 
 static enum led_brightness mdss_fb_get_bl_brightness(
