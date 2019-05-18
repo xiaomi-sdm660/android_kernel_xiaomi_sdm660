a6018e497e64bc805858c6234b22d8d9a4af7a29
Manish4586 <manish.n.manish45@gmail.com>
2019-05-18 10:44:41 +0000

    fbdev: mdss: Conform brightness soft limit
    Android usually restricts the minimum brightness for the panel in the
    
    closed interval of [0, 16]. This tweak allows the kernel to align the
    
    requested brightness level to a new slope, allowing brightness levels
    
    previously restricted.
     Author: Manish4586 <manish.n.manish45@gmail.com>
     Date:      Sat May 18 10:44:41 2019 +0000
    
    	modified:   drivers/video/fbdev/msm/mdss_dsi_panel.c

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -11,6 +11,7 @@
  */
 
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/interrupt.h>
 #include <linux/of.h>
 #include <linux/of_gpio.h>
@@ -43,6 +44,9 @@ extern bool enable_gesture_mode;
 
 bool ESD_TE_status = false;
 DEFINE_LED_TRIGGER(bl_led_trigger);
+
+static unsigned int bl_level_soft_limit = 16;
+module_param(bl_level_soft_limit, uint, 0644);
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -949,6 +953,22 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
+
+	/* Manipulate bl_level to conform to a user-space soft limit */
+	if (bl_level_soft_limit && bl_level) {
+		/* Store this as a percentage */
+		bl_level = bl_level * (pdata->panel_info.bl_max - pdata->panel_info.bl_min) / 
+			(pdata->panel_info.bl_max - bl_level_soft_limit);
+
+		/*
+		 * Subtract our soft limit to ensure the limits properly
+		 * fit in our bounds.
+		 */
+	if (bl_level > bl_level_soft_limit)
+		bl_level -= bl_level_soft_limit;
+	else
+		bl_level = pdata->panel_info.bl_min;
+	}
 
 	/*
 	 * Some backlight controllers specify a minimum duty cycle
