e206906e134310812b62712a74a3adcff4e0bd34
Tyler Nijmeh <tylernij@gmail.com>
2019-06-07 19:00:17 -0700

    drivers: touchscreen: nvt: Use a FIFO thread for work processing
    This should reduce any potential preemption by other kernel tasks when
    handling touch input work. See the Maxim STI touchscreen for
    inspiration.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- drivers/input/touchscreen/nvt_touch_e7s/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_e7s/nt36xxx.c
@@ -987,6 +987,9 @@ static void nvt_ts_work_func(void)
 #endif /* MT_PROTOCOL_B */
 	int32_t i = 0;
 	int32_t finger_cnt = 0;
+	struct sched_param param = { .sched_priority = MAX_USER_RT_PRIO / 2 };
+
+	sched_setscheduler(current, SCHED_FIFO, &param);
 
 	ret = CTP_I2C_READ(ts->client, I2C_FW_Address, point_data, POINT_DATA_LEN + 1);
 	if (unlikely(ret < 0)) {
