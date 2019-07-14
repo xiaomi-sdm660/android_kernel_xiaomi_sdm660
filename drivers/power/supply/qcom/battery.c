4df5034e31bad360a0be311ea63ee96ed4452212
Subhajeet Muhuri <kenny3fcb@gmail.com>
2019-07-14 12:36:46 +0530

    drivers: power: qcom: battery: Do not pr_info on pl_dbg

--- drivers/power/supply/qcom/battery.c
+++ drivers/power/supply/qcom/battery.c
@@ -91,13 +91,8 @@ enum print_reason {
 static int debug_mask = 0xff;
 module_param_named(debug_mask, debug_mask, int, S_IRUSR | S_IWUSR);
 
-#define pl_dbg(chip, reason, fmt, ...)				\
-	do {								\
-		if (debug_mask & (reason))				\
-			pr_info(fmt, ##__VA_ARGS__);	\
-		else							\
-			pr_debug(fmt, ##__VA_ARGS__);		\
-	} while (0)
+#define pl_dbg(chip, reason, fmt, ...)		\
+	pr_debug(fmt, ##__VA_ARGS__);		\
 
 enum {
 	VER = 0,
