0865fed47cfa59a03f4290d7759dfed9d38d8725
Subhajeet Muhuri <kenny3fcb@gmail.com>
2019-07-14 14:20:05 +0530

    drivers: mdss: Silence backlight logspam

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -958,7 +958,8 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 	 * for the backlight brightness. If the brightness is less
 	 * than it, the controller can malfunction.
 	 */
-	pr_err("%s: bl_level:%d\n", __func__, bl_level);
+
+	pr_debug("%s: bl_level:%d\n", __func__, bl_level);
 
 	/* do not allow backlight to change when panel in disable mode */
 	if (pdata->panel_disable_mode && (bl_level != 0))
