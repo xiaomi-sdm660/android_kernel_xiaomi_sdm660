da3c02c044804863497c6a538a4f36c9f0cfe80b
Subhajeet Muhuri <kenny3fcb@gmail.com>
2019-07-16 08:38:19 +0530

    drivers: leds: Import xiaomi changes

--- drivers/leds/leds-qpnp-wled.c
+++ drivers/leds/leds-qpnp-wled.c
@@ -572,7 +572,7 @@ static int qpnp_wled_set_level(struct qpnp_wled *wled, int level)
 {
 	int i, rc;
 	u8 reg;
-	u16 low_limit = WLED_MAX_LEVEL_4095 * 4 / 1000;
+	u16 low_limit = WLED_MAX_LEVEL_4095 * 1 / 1000;
 
 	/* WLED's lower limit of operation is 0.4% */
 	if (level > 0 && level < low_limit)
@@ -1519,10 +1519,6 @@ static irqreturn_t qpnp_wled_ovp_irq_handler(int irq, void *_wled)
 		pr_err("Error in reading WLED_FAULT_STATUS rc=%d\n", rc);
 		return IRQ_HANDLED;
 	}
-
-	if (fault_sts & (QPNP_WLED_OVP_FAULT_BIT | QPNP_WLED_ILIM_FAULT_BIT))
-		pr_err("WLED OVP fault detected, int_sts=%x fault_sts= %x\n",
-			int_sts, fault_sts);
 
 	if (fault_sts & QPNP_WLED_OVP_FAULT_BIT) {
 		if (wled->auto_calib_enabled && !wled->auto_calib_done) {
