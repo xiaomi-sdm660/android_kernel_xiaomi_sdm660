7b68adc3c92b3862f03782c949cfe6cd5f2d79dd
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-20 10:16:24 -0700

    qcacld-3.0: Enable debugfs usage for wahoo
    Wahoo userspace (libperfmgr) reads power_stats from debugfs.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_debugfs.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_debugfs.c
@@ -902,6 +902,10 @@ QDF_STATUS hdd_debugfs_init(hdd_adapter_t *adapter)
 	if (NULL == adapter->debugfs_phy)
 		return QDF_STATUS_E_FAILURE;
 
+#ifndef CONFIG_DEBUG_FS
+	return wlan_hdd_init_power_stats_debugfs(adapter);
+#endif
+
 	if (NULL == debugfs_create_file("wow_enable", 00400 | 00200,
 					adapter->debugfs_phy, adapter,
 					&fops_wowenable))
