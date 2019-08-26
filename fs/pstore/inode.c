14a2973f2873a7f1decb5f8e044ef83e0f543e5a
Max Weffers <rcstar6696@gmail.com>
2019-08-26 09:21:17 +0200

    ANDROID: pstore: Use vmalloc for large allocations due to ramoops size
    Android uses a 1 MiB console ramoops region, which requires kmalloc
    to be changed to vmalloc in the following places:
    
    1. pstore_mkfile(), allocation of inode->i_private
    2. ramoops_pstore_read(), allocation of buf
    
    Bug: 67383905
    Change-Id: Ie4f355a5991b7cb6ad356ded7bd9d41630602bf5
    Signed-off-by: Siqi Lin <siqilin@google.com>

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -36,6 +36,7 @@
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/uaccess.h>
+#include <linux/vmalloc.h>
 #include <linux/syslog.h>
 #ifdef CONFIG_PSTORE_LAST_KMSG
 #include <linux/proc_fs.h>
@@ -198,7 +199,7 @@ static void pstore_evict_inode(struct inode *inode)
 		spin_lock_irqsave(&allpstore_lock, flags);
 		list_del(&p->list);
 		spin_unlock_irqrestore(&allpstore_lock, flags);
-		kfree(p);
+		vfree(p);
 	}
 }
 
@@ -328,7 +329,7 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 		goto fail;
 	inode->i_mode = S_IFREG | 0444;
 	inode->i_fop = &pstore_file_operations;
-	private = kmalloc(sizeof *private + size, GFP_KERNEL);
+	private = vmalloc(sizeof *private + size);
 	if (!private)
 		goto fail_alloc;
 	private->type = type;
@@ -408,7 +409,7 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 
 fail_lockedalloc:
 	mutex_unlock(&d_inode(root)->i_mutex);
-	kfree(private);
+	vfree(private);
 fail_alloc:
 	iput(inode);
 
