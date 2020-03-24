f69a57fc4e6bfe534fcf5401f108db863083b07e
Dušan Uverić <dusan.uveric9@gmail.com>
2020-03-24 13:15:05 +0100

    power: supply: hook battery readings to quiet_therm for MI jason
    * Doing this since something is definitely wrong with battery temperature readings
    * Assuming CAF/Linux Stable merges broke this
    * This bug is also present on LOS kernel if my testing is correct
    
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -21,7 +21,7 @@
 #include <linux/platform_device.h>
 #include <linux/iio/consumer.h>
 #include <linux/qpnp/qpnp-revid.h>
-#ifdef CONFIG_MACH_LONGCHEER
+#if defined(CONFIG_MACH_LONGCHEER) || defined(CONFIG_MACH_XIAOMI_JASON)
 #include <linux/thermal.h>
 #endif
 #include "fg-core.h"
@@ -653,7 +653,7 @@ static int fg_get_battery_temp(struct fg_chip *chip, int *val)
 {
 	int rc = 0, temp;
 	u8 buf[2];
-#ifdef CONFIG_MACH_XIAOMI_TULIP
+#if defined(CONFIG_MACH_XIAOMI_TULIP) || defined(CONFIG_MACH_XIAOMI_JASON)
 	struct thermal_zone_device *quiet_them;
 #endif
 	rc = fg_read(chip, BATT_INFO_BATT_TEMP_LSB(chip), buf, 2);
@@ -729,6 +729,14 @@ static int fg_get_battery_temp(struct fg_chip *chip, int *val)
 	}
 #endif
 #endif
+
+#if defined(CONFIG_MACH_XIAOMI_JASON)
+       quiet_them = thermal_zone_get_zone_by_name("quiet_therm");
+       if (quiet_them)
+               rc = thermal_zone_get_temp(quiet_them, &temp);
+       temp = (temp - 3) * 10;
+#endif
+
 	*val = temp;
 	return 0;
 }
