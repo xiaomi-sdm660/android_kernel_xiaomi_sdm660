28efc2142cb37b7b040b4f616f129c900b121bdf
Max Weffers <rcstar6696@gmail.com>
2020-03-25 10:26:09 +0100

    fix issue

--- fs/exec.c
+++ fs/exec.c
@@ -1657,7 +1657,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (is_su && capable(CAP_SYS_ADMIN)) {
 		current->flags |= PF_SU;
 		su_exec();
-		
+  }
+
 	if (is_global_init(current->parent)) {
 		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
 			zygote32_sig = current->signal;
