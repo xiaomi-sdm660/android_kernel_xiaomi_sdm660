b6754e09632fa43f0845530d212df92de8b12bc3
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-04-06 20:29:06 +0530

    drivers: touchscreen: nvt: Disable debugging
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- drivers/input/touchscreen/nvt_touch_a/nt36xxx.h
+++ drivers/input/touchscreen/nvt_touch_a/nt36xxx.h
@@ -28,8 +28,6 @@
 
 #include "nt36xxx_mem_map.h"
 
-#define NVT_DEBUG 1
-
 
 #define NVTTOUCH_RST_PIN 66
 #define NVTTOUCH_INT_PIN 67
@@ -47,12 +45,8 @@
 #define I2C_FW_Address 0x01
 #define I2C_HW_Address 0x62
 
-#if NVT_DEBUG
-#define NVT_LOG(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
-#else
-#define NVT_LOG(fmt, args...)    pr_info("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
-#endif
-#define NVT_ERR(fmt, args...)    pr_err("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
+#define NVT_LOG(fmt, args...)    pr_debug("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
+#define NVT_ERR(fmt, args...)    pr_debug("[%s] %s %d: " fmt, NVT_I2C_NAME, __func__, __LINE__, ##args)
 
 
 #define NVT_TS_NAME "NVTCapacitiveTouchScreen"
