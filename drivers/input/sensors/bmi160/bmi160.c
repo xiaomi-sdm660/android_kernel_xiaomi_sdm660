c535bd912c849b6264460644f8412950d8e79f1f
Manish Sarkar <Manish.n.manish45@gmail.com>
2020-06-08 13:21:18 +0530

    drivers: sensors: bmi160: fix misleading-indentation
    ../drivers/input/sensors/bmi160/bmi160.c:17953:3: warning: misleading indentation; statement is not part of the previous 'if' [-Wmisleading-indentation]
                    p_bmi160->delay_msec(BMI160_YAS532_OFFSET_DELAY);
                    ^
    ../drivers/input/sensors/bmi160/bmi160.c:17951:2: note: previous statement is here
            if (p_bmi160->mag_manual_enable != BMI160_MANUAL_ENABLE)
            ^
    ../drivers/input/sensors/bmi160/bmi160.c:18468:4: warning: misleading indentation; statement is not part of the previous 'if' [-Wmisleading-indentation]
                            p_bmi160->delay_msec(BMI160_GEN_READ_WRITE_DELAY);
                            ^
    ../drivers/input/sensors/bmi160/bmi160.c:18465:2: note: previous statement is here
            if (p_bmi160->mag_manual_enable != BMI160_MANUAL_ENABLE)
            ^
    ../drivers/input/sensors/bmi160/bmi160.c:18485:3: warning: misleading indentation; statement is not part of the previous 'if' [-Wmisleading-indentation]
                    p_bmi160->delay_msec(BMI160_GEN_READ_WRITE_DELAY);
                    ^
    ../drivers/input/sensors/bmi160/bmi160.c:18482:2: note: previous statement is here
            if (p_bmi160->mag_manual_enable == BMI160_MANUAL_ENABLE)
            ^
    
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- drivers/input/sensors/bmi160/bmi160.c
+++ drivers/input/sensors/bmi160/bmi160.c
@@ -17171,9 +17171,11 @@ u8 v_mag_sec_if_pow_mode_u8)
 	}
 	/* set mag interface auto mode*/
 	if (p_bmi160->mag_manual_enable == BMI160_MANUAL_ENABLE)
+	{
 		com_rslt += bmi160_set_mag_manual_enable(
 		BMI160_MANUAL_DISABLE);
 		p_bmi160->delay_msec(BMI160_GEN_READ_WRITE_DELAY);
+	}
 	return com_rslt;
 }
 /*!
@@ -17948,8 +17950,10 @@ const s8 *p_offset_s8)
 	BMI160_RETURN_FUNCTION_TYPE com_rslt = E_BMI160_COMM_RES;
 
 	if (p_bmi160->mag_manual_enable != BMI160_MANUAL_ENABLE)
+	{
 		com_rslt = bmi160_set_mag_manual_enable(BMI160_MANUAL_ENABLE);
 		p_bmi160->delay_msec(BMI160_YAS532_OFFSET_DELAY);
+	}
 
 	    /* Write offset X data*/
 		com_rslt = bmi160_set_mag_write_data(p_offset_s8[0]);
@@ -18462,9 +18466,11 @@ u8 v_command_reg_data_u8)
 	BMI160_RETURN_FUNCTION_TYPE com_rslt = E_BMI160_COMM_RES;
 
 	if (p_bmi160->mag_manual_enable != BMI160_MANUAL_ENABLE)
+	{
 			com_rslt = bmi160_set_mag_manual_enable(
 			BMI160_MANUAL_ENABLE);
 			p_bmi160->delay_msec(BMI160_GEN_READ_WRITE_DELAY);
+	}
 
 		com_rslt = bmi160_set_mag_write_data(v_command_reg_data_u8);
 		p_bmi160->delay_msec(BMI160_GEN_READ_WRITE_DELAY);
@@ -18479,9 +18485,11 @@ u8 v_command_reg_data_u8)
 		p_bmi160->delay_msec(BMI160_GEN_READ_WRITE_DELAY);
 
 	if (p_bmi160->mag_manual_enable == BMI160_MANUAL_ENABLE)
+	{
 		com_rslt += bmi160_set_mag_manual_enable(
 		BMI160_MANUAL_DISABLE);
 		p_bmi160->delay_msec(BMI160_GEN_READ_WRITE_DELAY);
+	}
 
 	return com_rslt;
 
