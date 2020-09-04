824aeea2c540afad08df538584101358af281c25
Atndko <z1281552865@gmail.com>
2020-09-04 18:35:31 +0600

    include/linux/ (device-mapper/kernel) : Fix Clang-12 warnings
    I have tons of warnings when compilings kernel with Clang-12, like below one:
    
     ./include/linux/dcache.h:509:9: warning: '(' and '{' tokens introducing statement expression are separated by whitespace [-Wcompound-token-split-by-space]
              return mult_frac(val, sysctl_vfs_cache_pressure, 100);
                   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      ./include/linux/kernel.h:161:35: note: expanded from macro 'mult_frac'
    . #define mult_frac(x, numer, denom)(                     \
                                        ^~~~~~~~~~~~~~~~~~~~~~~
    
      ./include/linux/dcache.h:509:9: warning: '}' and ')' tokens terminating statement expression are separated by whitespace [-Wcompound-token-split-by-space]
              return mult_frac(val, sysctl_vfs_cache_pressure, 100);
                   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      ./include/linux/kernel.h:165:50: note: expanded from macro 'mult_frac'
              (quot * (numer)) + ((rem * (numer)) / (denom)); \
                                                            ^
    
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>
    Change-Id: I27c2c7352d438a92be66f598eee69640355390a8
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -54,12 +54,10 @@
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + __must_be_array(arr))
 
-#define u64_to_user_ptr(x) (		\
-{					\
+#define u64_to_user_ptr(x) ({		\
 	typecheck(u64, x);		\
 	(void __user *)(uintptr_t)x;	\
-}					\
-)
+})
 
 /*
  * This looks more complex than it should be. But we need to
@@ -83,58 +81,50 @@
 #endif
 
 /* The `const' in roundup() prevents gcc-3.3 from calling __divdi3 */
-#define roundup(x, y) (					\
-{							\
+#define roundup(x, y) ({				\
 	const typeof(y) __y = y;			\
 	(((x) + (__y - 1)) / __y) * __y;		\
-}							\
-)
-#define rounddown(x, y) (				\
-{							\
+})
+
+#define rounddown(x, y) ({				\
 	typeof(x) __x = (x);				\
 	__x - (__x % (y));				\
-}							\
-)
+})
 
 /*
  * Divide positive or negative dividend by positive divisor and round
  * to closest integer. Result is undefined for negative divisors and
  * for negative dividends if the divisor variable type is unsigned.
  */
-#define DIV_ROUND_CLOSEST(x, divisor)(			\
-{							\
+#define DIV_ROUND_CLOSEST(x, divisor)({		\
 	typeof(x) __x = x;				\
 	typeof(divisor) __d = divisor;			\
 	(((typeof(x))-1) > 0 ||				\
 	 ((typeof(divisor))-1) > 0 || (__x) > 0) ?	\
 		(((__x) + ((__d) / 2)) / (__d)) :	\
 		(((__x) - ((__d) / 2)) / (__d));	\
-}							\
-)
+})
+
 /*
  * Same as above but for u64 dividends. divisor must be a 32-bit
  * number.
  */
-#define DIV_ROUND_CLOSEST_ULL(x, divisor)(		\
-{							\
+#define DIV_ROUND_CLOSEST_ULL(x, divisor)({	 	\
 	typeof(divisor) __d = divisor;			\
 	unsigned long long _tmp = (x) + (__d) / 2;	\
 	do_div(_tmp, __d);				\
 	_tmp;						\
-}							\
-)
+})
 
 /*
  * Multiplies an integer by a fraction, while avoiding unnecessary
  * overflow or loss of precision.
  */
-#define mult_frac(x, numer, denom)(			\
-{							\
+#define mult_frac(x, numer, denom)({			\
 	typeof(x) quot = (x) / (denom);			\
 	typeof(x) rem  = (x) % (denom);			\
 	(quot * (numer)) + ((rem * (numer)) / (denom));	\
-}							\
-)
+})
 
 
 #define _RET_IP_		(unsigned long)__builtin_return_address(0)
@@ -144,14 +134,13 @@
 # include <asm/div64.h>
 # define sector_div(a, b) do_div(a, b)
 #else
-# define sector_div(n, b)( \
-{ \
+# define sector_div(n, b)({ \
 	int _res; \
 	_res = (n) % (b); \
 	(n) /= (b); \
 	_res; \
-} \
-)
+})
+
 #endif
 
 /**
