d0ae5c08c2cf83c0f824795febbf2581895006fd
Anush02198 <Anush.4376@gmail.com>
2020-11-25 13:43:57 +0530

    Revert "ASoC: msm: q6asm: silence logspam"
    This reverts commit c18248a6f23e693ebb18bf7dc873fcdcf5750b52.

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -2053,9 +2053,6 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 					&(session[session_id].session_lock),
 					flags);
 				return 0;
-			} else {
-				pr_debug("%s: payload size of %x is less than expected.\n",
-					__func__, data->payload_size);
 			}
 			if ((is_adsp_reg_event(payload[0]) >= 0) ||
 			    (payload[0] == ASM_STREAM_CMD_SET_PP_PARAMS_V2) ||
@@ -2128,7 +2125,7 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 							data->payload_size);
 				}
 			} else {
-				pr_debug("%s: payload size of %x is less than expected.\n",
+				pr_err("%s: payload size of %x is less than expected.\n",
 					__func__, data->payload_size);
 			}
 			break;
@@ -2144,7 +2141,7 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 				atomic_set(&ac->cmd_state_pp, payload[1]);
 				wake_up(&ac->cmd_wait);
 			} else {
-				pr_debug("%s: payload size of %x is less than expected.\n",
+				pr_err("%s: payload size of %x is less than expected.\n",
 					__func__, data->payload_size);
 			}
 			break;
@@ -2238,7 +2235,7 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 							payload[2],
 							payload[3]);
 				else
-					pr_debug("%s: payload size of %x is less than expected.\n",
+					pr_debug("%s: payload size of %u is less than expected.\n",
 						__func__,
 						data->payload_size);
 
@@ -2255,7 +2252,7 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 							 payload[4+i]);
 					}
 				} else {
-					pr_debug("%s: payload size of %x is less than expected.\n",
+					pr_err("%s: payload size of %x is less than expected.\n",
 						__func__, data->payload_size);
 				}
 				pr_debug("%s: callback size in ints = %i\n",
@@ -2361,7 +2358,7 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 				(uint64_t)(((uint64_t)payload[2] << 32) |
 					payload[1]);
 		} else {
-			dev_vdbg(ac->dev, "%s: payload size of %x is less than expected.n",
+			dev_err(ac->dev, "%s: payload size of %x is less than expected.\n",
 				__func__, data->payload_size);
 		}
 		if (atomic_cmpxchg(&ac->time_flag, 1, 0))
