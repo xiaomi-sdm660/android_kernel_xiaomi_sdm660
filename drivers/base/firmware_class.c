ubports-clover/android_kernel_xiaomi_clover

drivers/base: Import Xiaomi changes

drivers/base/firmware_class.c
@@ -312,6 +312,9 @@ static const char * const fw_path[] = {
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
 	"/lib/firmware",
+#ifdef CONFIG_XIAOMI_CLOVER
+	"/vendor/etc",
+#endif
 	"/lib64/firmware"
 };
 

xiaomi-sdm660/android_kernel_xiaomi_sdm660

drivers/base/firmware_class.c
@@ -312,6 +312,9 @@ static const char * const fw_path[] = {
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
 	"/lib/firmware",
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	"/vendor/etc",
+#endif
 	"/lib64/firmware"
 };
 